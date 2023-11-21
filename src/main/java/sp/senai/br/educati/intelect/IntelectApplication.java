c01cb5b5f596ab2b43021cda36a3a7036a42e54a
Pietro <pietromcosta2018@gmail.com>
2023-11-21 13:01:00 -0300

    arquivos jetbrain (projeto crud)

---
+++ src/main/java/sp/senai/br/educati/intelect/IntelectApplication.java
@@ -0,0 +1,13 @@
+package sp.senai.br.educati.intelect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntelectApplication.class, args);
+	}
+
+}
---
+++ src/main/java/sp/senai/br/educati/intelect/models/pessoa.java
@@ -0,0 +1,25 @@
+package sp.senai.br.educati.intelect.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(
+        name = "tipoPessoa",
+        length = 1,
+        discriminatorType = DiscriminatorType.STRING
+)
+
+
+public class pessoa {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+}
